29f70653205991409e7ece9cb9eece23acc2f654
LamiaaReda <94682700+LamiaaReda@users.noreply.github.com>
2021-12-25 13:16:36 +0200

    Add files via upload
    Add If Statement

--- Tip Calculator.js
+++ Tip Calculator.js
@@ -4,10 +4,11 @@ let people = document.querySelector(".people");
 let calculate = document.querySelector(".btn");
 let amount = document.querySelector(".tip");
 
-
-calculate.onclick = function(){
+calculate.onclick = function () {
+  if (bill.value == 0 || people.value == 0) {
+    alr = alert("please Enter All Entity");
+  } else {
     tip = (bill.value / people.value) * quality.value;
-    document.getElementById("Tip").innerHTML= tip;
-    console.log(tip);
-    return tip;
-}
+    document.getElementById("Tip").innerHTML = tip;
+  }
+};
